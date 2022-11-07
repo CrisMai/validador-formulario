d7057d63b6453da940bcdc347506605be217334a
CrisMai <crismai.dev@gmail.com>
2022-11-07 14:15:48 -0300

    Criando mensagem de erro nos campos do formulário

--- script.js
+++ script.js
@@ -6,22 +6,21 @@ let Validator = {
         let send = true;
         let inputs = form.querySelectorAll("input");
 
+        Validator.clearErros();
+
         for (let i=0; i<inputs.length; i++) {
             let input = inputs[i];
             let check = Validator.checkInput(input);
 
             if (check !== true) {
                 send = false;
-                console.log(check);
+                Validator.showError(input, check);
             }
-
         }
       
-
         if (send) {
             form.submit();
         }
-
     },
 
     checkInput: (input) => {
@@ -39,15 +38,41 @@ let Validator = {
 
                     break;
                     case 'min':
+                        if(input.value.length < rDetails[1]) {
+                            return "Campo tem que ter pelo menos "+rDetails[1]+" caracteres";
+                        }
                     
                     break;
                 }
             }
-
         }
         return true;
+    },
+
+    showError: (input, error) => {
+        input.style.borderColor = "#ff0000";
+
+        let errorElement = document.createElement("div");
+        errorElement.classList.add("error");
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+    },
+
+    clearErros: () => {
+
+        let inputs = form.querySelectorAll("input");
+        for(let i = 0; i < inputs.length; i++) {
+            inputs[i].style = "";
+        }
+
+        let errorElements = document.querySelectorAll(".error");
+        for(let i = 0; i < errorElements.length; i++) {
+            errorElements[i].remove();
+        }
+
     }
-};
+ };
 
 let form = document.querySelector(".validator");
 form.addEventListener("submit", Validator.handleSubmit);
