a1d4b897e35f5dbbeda89d76750b043264d0aea6
CrisMai <crismai.dev@gmail.com>
2022-11-08 13:12:21 -0300

    Criando validação do campo email - usando expressão regular

--- script.js
+++ script.js
@@ -40,9 +40,17 @@ let Validator = {
                     case 'min':
                         if(input.value.length < rDetails[1]) {
                             return "Campo tem que ter pelo menos "+rDetails[1]+" caracteres";
-                        }
-                    
+                        }   
                     break;
+                    case 'email':
+                        if (input.value != "") {
+                            let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                            if (!regex.test(input.value.toLowerCase())) {
+                                return "E-mail inválido!";
+
+                            }
+
+                        }
                 }
             }
         }
